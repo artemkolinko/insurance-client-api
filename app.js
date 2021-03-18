e4b9b60df9d570eddedd66a5a989cc7d3be618a0
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-03-18 18:05:00 +0200

    add jest, bring server init to bin/www

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 // const bodyParser = require('body-parser');
 const clientsRouter = require('./routes/clients');
-const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,9 +9,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1/clients', clientsRouter);
 
-// ============================================
-// Srart server
-// ============================================
-app.listen(PORT, () => {
-  console.warn(`Server has been started on port ${PORT}`);
-});
+module.exports = app;
