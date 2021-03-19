0a1f976ea79a0c5b12aa8ea26c5ecdf238bc10c5
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-03-19 13:22:34 +0200

    simple server start

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 // const bodyParser = require('body-parser');
 const clientsRouter = require('./routes/clients');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -9,4 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1/clients', clientsRouter);
 
-module.exports = app;
+// ============================================
+// Srart server
+// ============================================
+app.listen(PORT, () => {
+  console.warn(`Server has been started on port ${PORT}`);
+});
