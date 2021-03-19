54c6ba8b0e46f0ff89adbaf052aaedf1c0e35977
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-03-19 13:32:21 +0200

    Make package stucture (#4)
    * split to modules
    
    * modify .gitignore
    
    * add jest, bring server init to bin/www
    
    * add controller
    
    * fix run scripts
    
    * fix bugs
    
    * simple server start
    
    * fix main

---
+++ controller/clientController.js
@@ -0,0 +1,25 @@
+const { types } = require('cassandra-driver');
+const clientsDb = require('../models/clients');
+
+const clientTable = process.env.DB_CLIENT_TABLE;
+
+const selectAll = (table) => `SELECT * FROM ${table}`;
+const insert = (table) => `INSERT INTO ${table} (id, name, balance) VALUES (?, ?, ?)`;
+
+const getClients = (req, res) => {
+  clientsDb.execute(selectAll(clientTable))
+    .then(result => res.status(201).send(result.rows))
+    .catch(err => res.status(500).json({ err }));
+};
+
+const addClient = (req, res) => {
+  const id = types.timeuuid();
+  const { name } = req.body;
+  const balance = 0;
+  const params = [id, name, balance];
+  clientsDb.execute(insert(clientTable), params, { prepare: true })
+    .then(() => res.status(201).json({ id }))
+    .catch(err => res.status(500).json(err));
+};
+
+module.exports = { getClients, addClient };
