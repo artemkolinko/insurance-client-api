4f3293179401b4c10221b02f25bbe87b1b021e21
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-03-21 12:40:20 +0200

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 // const bodyParser = require('body-parser');
 const clientsRouter = require('./routes/clients');
 const productsRouter = require('./routes/products');
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1/clients', clientsRouter);
